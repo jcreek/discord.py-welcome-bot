ccd7bb056b70f59d5a7fb3e3c557c3ef9161312a
Mikey <terry.bazemoreiii@gmail.com>
2020-11-19 08:56:38 -0500

    Update welcome-bot.py

--- welcome-bot.py
+++ welcome-bot.py
@@ -4,9 +4,9 @@
 # import all necessary commands and libraries
 import discord
 import asyncio
-
 client=discord.Client()
-
+welcomechannel = await client.fetch_channel(channel_id) #https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID
+# Make sure you get the ID of your channel by right-clicking it and clicking `Copy ID`. Make sure developer mode is on!
 @client.event
 async def on_ready():
     print('logged in as')
@@ -28,20 +28,26 @@ here!
 async def on_member_join(member):
     print("Recognised that a member called " + member.name + " joined")
     try: 
-    await client.send_message(member, newUserMessage)
-    print("Sent message to " + member.name)
+        await client.send_message(member, newUserMessage)
+        print("Sent message to " + member.name)
     except:
         print("Couldn't message " + member.name)
-
-    # give member the steam role here
-    ## to do this the bot must have 'Manage Roles' permission on server, and role to add must be lower than bot's top role
-    role = discord.utils.get(member.server.roles, name="name-of-your-role")
-    await client.add_roles(member, role)
+    embed=discord.Embed(
+        title="Welcome "+member.name+"!"
+        description="We're so glad you're here!"
+        color=discord.Color.green()
+    )
+        
+    role = discord.utils.get(member.server.roles, name="name-of-your-role") # Gets the member role as a `role` object
+    await client.add_roles(member, role) # Gives the role to the user
     print("Added role '" + role.name + "' to " + member.name)
 
+@client.event
+async def on_member_leave(member):
+    print("Recognised that a member called " + member.name + " left")
+    embed=discord.Embed(
+        title="😢 Goodbye "+member.name+"!",
+        description="Until we meet again old friend." #A description isn't necessary, you can delete this line if you don't want a description.
+        color=discord.Color.red() #There are lots of colors, you can check them here: https://discordpy.readthedocs.io/en/latest/api.html?highlight=discord%20color#discord.Colour
+    )
 client.run('token') 
-
-
-
-
-
